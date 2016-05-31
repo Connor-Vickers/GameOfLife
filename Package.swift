cfbd78ad2e0f89b6663ed98c69f80d364be1099b
hkgumbs <kofi.gumbs@raise.com>
2016-05-31 18:15:55 -0500

    Move tests to Source/Specs

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   targets: [
     Target(name: "Core"),
     Target(name: "Main", dependencies: [.Target(name: "Core")]),
-    Target(name: "CoreTest", dependencies: [.Target(name: "Core")])
+    Target(name: "Specs", dependencies: [.Target(name: "Core")])
   ],
   dependencies: [
     .Package(url: "https://github.com/hkgumbs/XTest.git", Version(0,1,1))
