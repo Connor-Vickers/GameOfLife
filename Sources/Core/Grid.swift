e6e09670fa7217d44c3babb60f9c0b060755e119
Connor Vickers <connor@Connors-MBP.hsd1.il.comcast.net>
2016-08-03 15:00:24 -0500

    added tick functionality

--- Sources/Core/Grid.swift
+++ Sources/Core/Grid.swift
@@ -1,34 +1,172 @@
 import Foundation
 
 public class Grid {
-	var cells: [String]
+	var cells: [Bool]
 	var numberOfRows: Int { return self.getNumberOfRows() }
 	var numberOfColumns: Int { return self.getNumberOfCols() }
 	let input: String
+
+	static let aliveSymbol = "*"
+	static let deadSymbol = " "
 
 	public init(file: String){
 		input = file
 		cells = []
 		for character in file.replacingOccurrences(of: "\n", with: "").characters{
-			cells.append(String(character))
+			cells.append(charToBool(char: String(character)))
 		}
-		print(cells)
+	}
 
-	}
 	public func write() -> String{
-		return cells.reduce(""){ acc, element in			
-			return acc.appending(element)
+		var toWrite = ""
+		for row in 0..<numberOfRows {
+			for column in 0..<numberOfColumns{
+				let bool = cells[row*numberOfColumns + column]
+				toWrite.append(boolToChar(bool: bool))
+			}
+			if row != numberOfRows - 1 {
+				toWrite.append("\n")
+			}
 		}
+		return toWrite
 	}
+
 	public func getNumberOfRows() -> Int{
 		return input.components(separatedBy: "\n").count
 	}
+
 	public func getNumberOfCols() -> Int{
 		return input.components(separatedBy: "\n")[0].characters.count 
 	}
+
 	public static func isValid(file: String) -> Bool {
 		var lengths = file.components(separatedBy: "\n").map(){$0.characters.count}
 		lengths.sort()
 		return lengths.first == lengths.last
 	}
+
+	func charToBool(char: String) -> Bool{
+		switch(char){
+			case Grid.aliveSymbol:
+				return true
+			case Grid.deadSymbol:
+				return false
+			default:
+				assert(false, "invalid cell")
+		}
+    }
+
+    func boolToChar(bool: Bool) -> String{
+		return (bool ? Grid.aliveSymbol : Grid.deadSymbol)
+    }
+
+    static func isNextCellAlive(currentCellIsAlive: Bool, numberOfNeighbors: Int) -> Bool{
+    	if currentCellIsAlive {
+    		if rule1(numberOfNeighbors: numberOfNeighbors) {
+    			return false
+    		}else if rule2(numberOfNeighbors: numberOfNeighbors) {
+    			return true
+    		}else{ // numberOfNeighbors is > 3 therefore rule 3 applies
+    			return false
+    		}
+    	}else{
+    		return rule4(numberOfNeighbors: numberOfNeighbors)
+    	}
+    }
+
+    static func rule1(numberOfNeighbors: Int) -> Bool {
+    	return numberOfNeighbors < 2
+    }
+	static func rule2(numberOfNeighbors: Int) -> Bool {
+    	return numberOfNeighbors == 2 || numberOfNeighbors == 3
+    }
+    static func rule4(numberOfNeighbors: Int) -> Bool {
+    	return numberOfNeighbors == 3
+    }
+
+    func indexesOfNeighbors(forIndex: Int) -> [Int] {
+    	var possibleNeighbors: [String: Int] = [:]
+    	possibleNeighbors["top"] = forIndex - numberOfColumns
+    	possibleNeighbors["bottom"] = forIndex + numberOfColumns
+    	possibleNeighbors["left"] = forIndex - 1
+    	possibleNeighbors["right"] = forIndex + 1
+    	possibleNeighbors["topLeft"] = forIndex - numberOfColumns - 1
+    	possibleNeighbors["topRight"] = forIndex - numberOfColumns + 1
+    	possibleNeighbors["bottomLeft"] = forIndex + numberOfColumns - 1
+    	possibleNeighbors["bottomRight"] = forIndex + numberOfColumns + 1
+    	let validNeibors = removeInvalidNeighbors(index: forIndex, neighbors: possibleNeighbors) 
+    	return Array(validNeibors.values)
+    }
+
+    func removeInvalidNeighbors(index: Int, neighbors: [String: Int]) -> [String:Int]{
+    	var neighbors = neighbors
+    	if isTopRow(index: index){
+    		neighbors.removeValue(forKey: "topLeft")
+    		neighbors.removeValue(forKey: "top")
+    		neighbors.removeValue(forKey: "topRight")
+    	}
+    	if isBottomRow(index: index){
+    		neighbors.removeValue(forKey: "bottomLeft")
+    		neighbors.removeValue(forKey: "bottom")
+    		neighbors.removeValue(forKey: "bottomRight")
+    	}
+    	if isLeftColumn(index: index){
+    		neighbors.removeValue(forKey: "topLeft")
+    		neighbors.removeValue(forKey: "left")
+    		neighbors.removeValue(forKey: "bottomLeft")
+    	}
+    	if isRightColumn(index: index){
+    		neighbors.removeValue(forKey: "topRight")
+    		neighbors.removeValue(forKey: "right")
+    		neighbors.removeValue(forKey: "bottomRight")
+    	}
+    	return neighbors
+    }
+
+    func isTopRow(index: Int) -> Bool {
+    	return index < numberOfColumns
+    }
+    func isBottomRow(index: Int) -> Bool {
+    	return index >= numberOfColumns * (numberOfRows - 1)
+    }
+    func isLeftColumn(index: Int) -> Bool {
+    	return index % numberOfColumns == 0
+    }
+    func isRightColumn(index: Int) -> Bool {
+    	return (index + 1) % numberOfColumns == 0
+    }
+
+    func numberOfAliveNeighbors(forIndex: Int) -> Int{
+    	var aliveNeighbors = 0
+    	for indexesOfNeighbor in indexesOfNeighbors(forIndex: forIndex){
+    		if cells[indexesOfNeighbor]{
+    			aliveNeighbors += 1
+    		}
+    	}
+    	return aliveNeighbors
+    }
+
+    public func tick(){
+    	 var newCells = cells
+    	 var indexesOfCellsToConsider: [Int] = []
+    	 for index in 0..<cells.count{
+    	 	if cells[index]{
+    	 		indexesOfCellsToConsider.append(index)
+    			indexesOfCellsToConsider.append(contentsOf: indexesOfNeighbors(forIndex: index))
+    	 	}
+    	 }
+    	 for index in Set(indexesOfCellsToConsider){
+    	 	let numberOfNeighbors = numberOfAliveNeighbors(forIndex: index)
+         	newCells[index] = Grid.isNextCellAlive(currentCellIsAlive: cells[index], numberOfNeighbors: numberOfNeighbors)
+    	 }
+    	 cells = newCells
+    }
+
+
+
+
+
+
+
+
 }
--- Sources/Specs/GridSpec.swift
+++ Sources/Specs/GridSpec.swift
@@ -1,17 +1,24 @@
 import Speck
-import Core
+@testable import Core
+
+let deadSymbol = Grid.deadSymbol
+let aliveSymbol = Grid.aliveSymbol
 
 let GridSpec = describe("grid") {
   it("it can recieve and write a file") {
-    let grid = Grid(file: "hello")
-  	expect(grid.write()).to(equal: "hello")
+    let grid = Grid(file: "\(aliveSymbol)\(aliveSymbol)\(deadSymbol)\(deadSymbol)\(aliveSymbol)")
+  	expect(grid.write()).to(equal: "\(aliveSymbol)\(aliveSymbol)\(deadSymbol)\(deadSymbol)\(aliveSymbol)")
+  }
+  it("it can recieve and write a file with multiable lines") {
+    let grid = Grid(file: "\(aliveSymbol)\(aliveSymbol)\n\(deadSymbol)\(deadSymbol)\n\(aliveSymbol)\(aliveSymbol)")
+  	expect(grid.write()).to(equal: "\(aliveSymbol)\(aliveSymbol)\n\(deadSymbol)\(deadSymbol)\n\(aliveSymbol)\(aliveSymbol)")
   }
   it("detects how many rows there are") {
-  	let grid = Grid(file: "row1\nrow2\nrow3")
+  	let grid = Grid(file: "\(aliveSymbol)\(aliveSymbol)\n\(deadSymbol)\(deadSymbol)\n\(aliveSymbol)\(aliveSymbol)")
   	expect(grid.getNumberOfRows()).to(equal: 3)
   }
   it("detects how many columns there are") {
-  	let grid = Grid(file: "row1\nrow2\nrow3")
+  	let grid = Grid(file: "\(aliveSymbol)\(deadSymbol)\(aliveSymbol)\(deadSymbol)\n\(deadSymbol)\(deadSymbol)\(aliveSymbol)\(deadSymbol)\n\(aliveSymbol)\(deadSymbol)\(deadSymbol)\(aliveSymbol)")
   	expect(grid.getNumberOfCols()).to(equal: 4)
   }
   it("detects when row lengths are not the same") {
@@ -19,4 +26,22 @@ let GridSpec = describe("grid") {
   	expect(Grid.isValid(file: "row1\now2\nrow3")).to(equal: false)
   	expect(Grid.isValid(file: "row1\nrow2\nrow3\n")).to(equal: false)
   }
+  it("determines if a cell should live or die") {
+  	expect(Grid.isNextCellAlive(currentCellIsAlive: false, numberOfNeighbors: 0)).to(equal: false)  	
+  	expect(Grid.isNextCellAlive(currentCellIsAlive: true, numberOfNeighbors: 1)).to(equal: false)
+	expect(Grid.isNextCellAlive(currentCellIsAlive: true, numberOfNeighbors: 2)).to(equal: true)
+	expect(Grid.isNextCellAlive(currentCellIsAlive: true, numberOfNeighbors: 3)).to(equal: true)
+	expect(Grid.isNextCellAlive(currentCellIsAlive: true, numberOfNeighbors: 4)).to(equal: false)
+	expect(Grid.isNextCellAlive(currentCellIsAlive: false, numberOfNeighbors: 2)).to(equal: false)
+	expect(Grid.isNextCellAlive(currentCellIsAlive: false, numberOfNeighbors: 3)).to(equal: true)  	
+	expect(Grid.isNextCellAlive(currentCellIsAlive: false, numberOfNeighbors: 4)).to(equal: false)  	
+  }
+   it("can step one step") {
+   	let grid = Grid(file: "   \n***\n   ")
+   	grid.tick()
+   	grid.tick()
+   	grid.tick()
+   	expect(grid.write()).to(equal: " * \n * \n * ")
+   }
 }
+
